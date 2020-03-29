1d1752ceb404cfa4eaf45e39b62c0537e9d83206
Shane Duncan <aequitas321@icloud.com>
2020-03-28 23:11:39 -0600

    Added new methods and a menu

--- src/com/skilldistillery/foodtruck/FoodTruck.java
+++ src/com/skilldistillery/foodtruck/FoodTruck.java
@@ -3,31 +3,78 @@ package com.skilldistillery.foodtruck;
 import java.util.Scanner;
 
 public class FoodTruck {
-	Scanner kb = new Scanner(System.in);
-	String[] foodTrucks = new String[5];
 
-	public void input() {
+	private String truckName;
+	private int truckRating;
+	private String foodType;
 
-		int i = 0;
-		String truckInfo = "";
-
-		while (i < 5) {
-			System.out.println("Enter your food trucks, You must have at least 3.");
-			System.out.println("You have " + i + " trucks now.");
-//			System.out.println("Truck" + (i + 1) + ": ");
-			truckInfo = kb.nextLine();
-			if (i >= 3 && truckInfo.equalsIgnoreCase("quit")) {
-				break;
-			} else if (i < 3 && truckInfo.equalsIgnoreCase("quit")) {
-				System.out.println("You must have at least 3 trucks!");
-			} else {
-				foodTrucks[i] = truckInfo;
-				i++;
-			}
-
-		}
-		System.out.println("You now have " + i + " trucks.");
-		System.out.println(foodTrucks.toString());
+	public FoodTruck(String truckName, int truckRating, String foodType) {
+		this.truckName = truckName;
+		this.truckRating = truckRating;
+		this.foodType = foodType;
 	}
+
+
+	public String getTruckName() {
+		return truckName;
+	}
+
+	public void setTruckName(String truckName) {
+		this.truckName = truckName;
+	}
+
+	public int getTruckRating() {
+		return truckRating;
+	}
+
+	public void setTruckRating(int truckRating) {
+		this.truckRating = truckRating;
+	}
+
+	public String getFoodType() {
+		return foodType;
+	}
+
+	public void setFoodType(String foodType) {
+		this.foodType = foodType;
+	}
+
+
+	@Override
+	public String toString() {
+		return "FoodTruck [truckName=" + truckName + ", truckRating=" + truckRating + ", foodType=" + foodType + "]";
+	}
+
+	
+	
+//	public void input() {
+//		String userTruckName;
+//		int userTruckRating;
+//		String userFoodType;
+//		int i = 0;
+//
+//		while (i < 5) {
+//			System.out.println("Enter your food trucks, You must have at least 3.");
+//			System.out.println("You have " + i + " trucks now.");
+////			System.out.println("Truck" + (i + 1) + ": ");
+//			System.out.println("What is the truck name?");
+//			userTruckName = kb.nextLine();
+//			System.out.println("What type of food does this truck have?");
+//			userFoodType = kb.nextLine();
+//			System.out.println("What is the rating of this truck? (Between 1 & 5)");
+//			userTruckRating = kb.nextInt();
+//			if (i >= 3 && truckName.equalsIgnoreCase("quit")) {
+//				break;
+//			} else if (i < 3 && truckName.equalsIgnoreCase("quit")) {
+//				System.out.println("You must have at least 3 trucks!");
+//			} else {
+//				foodTrucks[i] = new FoodTruck(userTruckName, userTruckRating, userFoodType).toString();
+//				i++;
+//			}
+//
+//		}
+//		System.out.println("You now have " + i + " trucks.");
+//		System.out.println(foodTrucks.toString());
+//	}
 
 }
--- src/com/skilldistillery/foodtruck/FoodTruckApplication.java
+++ src/com/skilldistillery/foodtruck/FoodTruckApplication.java
@@ -1,18 +1,110 @@
 package com.skilldistillery.foodtruck;
 
+import java.util.Scanner;
+
 public class FoodTruckApplication {
 
+	private Scanner kb = new Scanner(System.in);
+	
 	public static void main(String[] args) {
-		FoodTruck one = new FoodTruck();
+		FoodTruckApplication fta = new FoodTruckApplication();
+		FoodTruck[] foodtrucks = fta.createFoodTrucks();
 		
+		while(true) {
+			fta.printMenu(foodtrucks);
+		}
+
+	}
+
+	
+
+	public FoodTruck[] createFoodTrucks() {
+		FoodTruck[] foodTrucks = new FoodTruck[5];
+		String userTruckName;
+		int userTruckRating;
+		String userFoodType;
+		int i = 0;
+
+		while (i < 5) {
+			
+			System.out.println("Enter your food trucks, You must have at least 3.");
+			System.out.println("You have " + i + " trucks now.");
+//			System.out.println("Truck" + (i + 1) + ": ");
+			System.out.println("\nWhat is the truck name?");
+			userTruckName = kb.nextLine();
+			
+			if (i >= 3 && userTruckName.equalsIgnoreCase("quit")) {
+				break;
+				
+			} else if (i < 3 && userTruckName.toLowerCase().contains("quit")) {
+				System.out.println("You must have at least 3 trucks!");
+				
+			}
+			
+			System.out.println("\nWhat is the rating of this truck? (Between 1 & 5)");
+			userTruckRating = Integer.parseInt(kb.nextLine());
+
+			System.out.println("\nWhat type of food does this truck have?");
+			userFoodType = kb.nextLine();
+
+			foodTrucks[i] = new FoodTruck(userTruckName, userTruckRating, userFoodType);
+			i++;
+
+		}
 		
+		return foodTrucks;
 
 	}
 	
+	public void printMenu(FoodTruck[] foodTrucks) {
+		
+		System.out.println("=========================");
+		System.out.println("\t\tMenu");
+		System.out.println("=========================");
+		System.out.println("1) - List Trucks");
+		System.out.println("2) - Average Rating");
+		System.out.println("3) - I dont know");
+		System.out.println("4) - I dont know");
+		System.out.println("5) - Quit");
+		System.out.println("-------------------------");
+		System.out.print("choice: ");
+		
+		
+			switch(kb.next()) {
+			case "1":
+				listTrucks(foodTrucks);
+				break;
+			case "2":
+				averageRating(foodTrucks);
+				break;
+			case "3":
+				break;
+			case "4":
+				break;
+			case "5":
+				System.exit(0);
+				default:
+					System.out.println("* Invalid choice, Try again.");
+					break;
+			}
+			
+			
+	}
 	
 	
-	public void run() {
-		System.out.println("What would you like to do?3");
+	public void listTrucks(FoodTruck[] foodTrucks) {
+		System.out.println("You now have " + foodTrucks.length + " trucks.");
+		for (FoodTruck ft : foodTrucks) {
+			if(ft == null) {
+				System.out.println("[Empty]");
+				continue;
+			}
+			System.out.println(ft.toString());
+		}
+	}
+	
+	public void averageRating(FoodTruck[] foodTrucks) {
+		System.out.println("HEY I DIDN't DO THIS YET");
 	}
 
 }
